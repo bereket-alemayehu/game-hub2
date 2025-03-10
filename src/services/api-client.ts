2ea4752bc91affbef52d869045e1ebf90ae2ec55
Bereket <bereketalemayehuf@gmail.com>
2025-03-10 15:15:06 -0500

    Displaying platform icons

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "58766e3b70e6447fa8b070a17a73d349",
+    key: "94761b380f5648669e412ad648da8bd0",
   },
 });
