b450cb0b9778581c76eccbfabefd559e0e8abc2c
Bereket <bereketalemayehuf@gmail.com>
2025-03-12 09:07:55 -0500

    handling games with no image

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,6 +1,6 @@
+import { AxiosRequestConfig, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   count: number;
@@ -11,13 +11,13 @@ const useData = <T>(
   requestConfig?: AxiosRequestConfig,
   deps?: any[]
 ) => {
-  const controller = new AbortController();
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState(" ");
   const [isLoading, setLoading] = useState(false);
 
   useEffect(
     () => {
+      const controller = new AbortController();
       setLoading(true);
       apiClient
         .get<FetchResponse<T>>(endpoint, {
@@ -29,9 +29,9 @@ const useData = <T>(
           setLoading(false);
         })
         .catch((err) => {
+          if (err instanceof CanceledError) return;
           setError(err.message);
           setLoading(false);
-          if (err instanceof CanceledError) return;
         });
 
       return () => controller.abort();
