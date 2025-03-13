b9e3823cd3b038ea0721f7fccf591cc9aa6081f0
Bereket <bereketalemayehuf@gmail.com>
2025-03-13 01:13:40 -0500

    customize the theme to get darker grays

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -21,6 +21,7 @@ const SearchInput = ({ onSearch }: Props) => {
           borderRadius={20}
           placeholder="Search games..."
           variant="fillded"
+          // bg="gray.700"
         />
       </InputGroup>
     </form>
