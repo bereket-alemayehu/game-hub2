14ed944d8f81a86c1509ea44fd12e8e73525eb09
Bereket <bereketalemayehuf@gmail.com>
2025-03-17 10:33:48 -0500

    Refactoring fetch games with react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
 
 export interface Genre {
   id: number;
@@ -6,6 +9,16 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null });
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
 
+    staleTime: 24 * 60 * 60 * 1000, //24h so the data is fresh for 24hr and no need to send a request to fetch data from backend.
+    initialData: {
+      count: genres.length,
+      results: genres,
+    },
+  });
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,25 @@
-import platforms from "../data/platform";
-
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import platform from "../data/platform";
+import { FetchResponse } from "../services/api-client";
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: {
+      count: platform.length,
+      results: platform,
+    },
+  });
 
 export default usePlatforms;
